b58de80c041d30fd333d4c56d528810529017d3b
Maksym Chubarov <maximuskh@gmail.com>
2021-02-20 23:11:44 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
+import user from './components/Profile/user.json';
+import statisticalData from './components/Statistics/statistical-data.json';
+import friends from './components/FriendList/friends.json';
+
+
+
+const App = () => {
+
+return(
+<>
+    <Profile
+    name={user.name}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+    />
+    
+    <Statistics title="Upload stats" stats={statisticalData} />
+
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />;
+
+</>
+
+
+
+)}; 
+
+export default App;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './profile.module.css';
+
+
+const Profile = ({name, tag, location, avatar, stats}) =>{
+    
+return(
+<div className={styles.profile}>
+
+    <div className={styles.description}>
+    <img
+        src= {avatar}
+        alt="Аватар пользователя"
+        className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+
+    </div>
+
+
+  <ul className={styles.stats}>
+    <li>
+      <span className={styles.label}>Followers</span>
+      <span className={styles.quantity}>{stats.followers}</span>
+    </li>
+    <li>
+      <span className={styles.label}>Views</span>
+      <span className={styles.quantity}>{stats.views}</span>
+    </li>
+    <li>
+      <span className={styles.label}>Likes</span>
+      <span className={styles.quantity}>{stats.likes}</span>
+    </li>
+  </ul>
+
+</div>
+
+
+)
+}
+Profile.defaultProps = {
+    avatar: 'https://ipbmafia.ru/uploads/monthly_2018_07/895242-200.png.e10304d04e80f56d3ebaa863b4ccdd41.png',
+}; 
+// Profile.propTypes
+
+export default Profile;
